6e2776a3ad32be3100fee254a58173ef684b984d
chenjianrui <sky75217@gmail.com>
2020-08-27 22:27:43 +0800

    粗略的測試

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />)
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1)
 });
+
+test('renders a display counter', () => {
+  const wrapper = shallow(<App />)
+  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  expect(counterDisplay.length).toBe(1)
+})
+
+test('renders a increment button', () => {
+  const wrapper = shallow(<App />)
+  const incrementButton = wrapper.find("[data-test='increment-button']")
+  expect(incrementButton.length).toBe(1)
+})
