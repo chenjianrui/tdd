06bb85ea313fe2416966830e82081411961cbe70
chenjianrui <sky75217@gmail.com>
2020-09-03 22:39:16 +0800

    add successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,17 @@
-export default (state, action) => {
-  return null
+import { actionTypes } from '../actions/index'
+
+const initialState = {
+  success: false
+}
+
+export default (state = initialState, action) => {
+  switch(action.type){
+    case actionTypes.CORRECT_GUESS:
+      return {
+        ...state,
+        success: true
+      }
+    default:
+      return state
+  }
 }
---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,10 @@
+import successReducer from './successReducer'
+import { actionTypes } from '../actions/index'
+test('returns default initial state of `false` when no action is passed', () => {
+  const newState = successReducer(undefined, {})
+  expect(newState).toEqual({ success: false })
+})
+test('returns state of true upon receiving an action of type `CORRECT_GUESS`', () => {
+  const newState = successReducer(undefined, { type: actionTypes.CORRECT_GUESS })
+  expect(newState).toEqual({ success: true })
+})
