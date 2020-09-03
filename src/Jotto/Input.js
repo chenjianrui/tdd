c86a6fde31613a8288c42dd1d3f27e9ea2d846dc
chenjianrui <sky75217@gmail.com>
2020-09-03 23:33:20 +0800

    新增 Input Component & test

---
+++ src/Jotto/Input.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Input extends Component {
+  render () {
+    return (
+      <div />
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {}
+}
+
+export default connect(mapStateToProps)(Input)
---
+++ src/Jotto/Input.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findByTestAttr } from '../test/testUtil'
+import Input from './Input'
+
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    test('renders component without error', () => {
+
+    })
+    test('renders input box', () => {
+
+    })
+    test('renders submit button', () => {
+      
+    })
+  })
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {
+
+    })
+    test('does not render input box', () => {
+
+    })
+    test('does not render submit button', () => {
+
+    })
+  })
+})
+describe('update state', () => {
+
+})
